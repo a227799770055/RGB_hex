34a8b1b7947ae025a9de44ce5bfd3db2a2bf9367
Eddie Hsiao <a227799770055@gmail.com>
2020-03-25 21:21:43 +0800

    hex_transfer

--- index.js
+++ index.js
@@ -8,6 +8,25 @@
   const input_slider = document.querySelector(".slider")
   const color_panel = document.querySelector("#color_panel")
   const body = document.querySelector("body")
+  const hex_show = ""
+  const hex = {
+    0: "0",
+    1: "1",
+    2: "2",
+    3: "3",
+    4: "4",
+    5: '5',
+    6: "6",
+    7: "7",
+    8: "8",
+    9: "9",
+    10: "A",
+    11: "B",
+    12: "C",
+    13: "D",
+    14: "E",
+    15: "F"
+  }
 
 
 
@@ -16,14 +35,20 @@
   green_value.innerHTML = `<span>${green_input.value}</span>`
 
 
-  input_slider.addEventListener('mouseover', event => {
+  input_slider.addEventListener('mousemove', event => {
     if (event.target.matches("input")) {
       body.style = `background-color:rgb(${red_input.value},${green_input.value},${blue_input.value})`
       red_value.innerHTML = `<span>${red_input.value}</span>`
       blue_value.innerHTML = `<span>${blue_input.value}</span>`
       green_value.innerHTML = `<span>${green_input.value}</span>`
-      color_panel.innerHTML = `<h1 style="background-color:rgb(145, 136, 136, 0.5)">RGB ={${red_input.value},${green_input.value},${blue_input.value}}</h1>`
+      color_panel.innerHTML = `<h1 style="background-color:rgb(145, 136, 136, 0.5)">RGB = #${hexTransfer(red_input.value)}${hexTransfer(green_input.value)}${hexTransfer(blue_input.value)}</h1>`
     }
   })
+
+  function hexTransfer(value) {
+    hex_first = hex[Math.floor(value / 16)]
+    hex_second = hex[value % 16]
+    return hex_first += hex_second
+  }
 })()
 
